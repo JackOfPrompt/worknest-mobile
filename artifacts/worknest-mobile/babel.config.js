e9ee56ff7fa6e57444c6e3ff15aca9e362f704e4
Akhil_Tadala <tadalaakhilmech@gmail.com>
2026-03-20 12:18:36 +0530

    Update babel.config.js with module-resolver

--- artifacts/worknest-mobile/babel.config.js
+++ artifacts/worknest-mobile/babel.config.js
@@ -1,6 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', { root: ['./'], alias: { '@': './src' } }],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
