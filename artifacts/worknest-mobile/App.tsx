5def6508de2755dc8b9c6ef9e296fd9a26958fe0
Akhil_Tadala <tadalaakhilmech@gmail.com>
2026-03-20 12:18:37 +0530

    Add App.tsx root entry point

---
+++ artifacts/worknest-mobile/App.tsx
@@ -0,0 +1,21 @@
+import 'react-native-url-polyfill/auto'
+import 'react-native-gesture-handler'
+import React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { AuthProvider } from '@/context/AuthContext'
+import { RootNavigator } from '@/navigation/RootNavigator'
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <StatusBar style="auto" />
+          <RootNavigator />
+        </AuthProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  )
+}
